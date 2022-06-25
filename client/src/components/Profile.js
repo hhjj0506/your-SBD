cff40e04562f6567f39620ebc5052f51c4c59ba3
Hyeongjin Kim <hhjj0506@gmail.com>
2022-06-25 22:32:03 +0900

    get current user info

--- client/src/components/Profile.js
+++ client/src/components/Profile.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import axios from 'axios';
 import jwt_decode from "jwt-decode";
 import { useNavigate } from "react-router-dom";
@@ -8,10 +8,31 @@ const Profile = () => {
     const [email, setEmail] = useState('');
     const [token, setToken] = useState('');
     const [expire, setExpire] = useState('');
+    const [users, setUsers] = useState([]);
     const history = useNavigate();
 
-    const axiosJWT = axios.create();
+    useEffect(() => {
+        refreshToken();
+        getUsers();
+    }, []);
  
+    const refreshToken = async () => {
+        try {
+            const response = await axios.get('http://localhost:5000/token');
+            setToken(response.data.accessToken);
+            const decoded = jwt_decode(response.data.accessToken);
+            setName(decoded.name);
+            setEmail(decoded.email);
+            setExpire(decoded.exp);
+        } catch (error) {
+            if (error.response) {
+                history("/");
+            }
+        }
+    }
+
+    const axiosJWT = axios.create();
+
     axiosJWT.interceptors.request.use(async (config) => {
         const currentDate = new Date();
         if (expire * 1000 < currentDate.getTime()) {
@@ -27,6 +48,15 @@ const Profile = () => {
     }, (error) => {
         return Promise.reject(error);
     });
+
+    const getUsers = async () => {
+        const response = await axiosJWT.get('http://localhost:5000/users', {
+            headers: {
+                Authorization: `Bearer ${token}`
+            }
+        });
+        setUsers(response.data);
+    }
 
     return (
         <div>
